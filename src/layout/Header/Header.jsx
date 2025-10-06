936db3ad61a08b706df8e93fd890d63151d848d6
LitanMolla <litanmolla9@gmail.com>
2025-10-06 14:04:43 +0600

    mobile menu added

--- src/layout/Header/Header.jsx
+++ src/layout/Header/Header.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Container from '../../components/Container/Container'
 import { Link, NavLink } from 'react-router'
 import { Menu } from 'lucide-react'
 
 const Header = () => {
+  const [isOpen, setisOpen] = useState(true);
   return (
     <>
-      <header className='py-3'>
+      <header className='py-3 shadow mb-5'>
         <Container>
           <div className="flex items-center justify-between">
             <Link to={'/'} className='text-2xl font-semibold'>Book Vibe</Link>
@@ -20,8 +21,17 @@ const Header = () => {
               <Link className='bg-cyan-500 text-gray-100 px-5 py-2 rounded-md cursor-pointer border duration-300 hover:bg-cyan-600'>Register</Link>
             </div>
             {/* bar btn */}
-            <Link className='md:hidden'><Menu /></Link>
+            <Link onClick={()=>setisOpen(prv=>!prv)} className='md:hidden'><Menu /></Link>
           </div>
+          {
+            isOpen
+            &&
+            <ul className='text-center mt-5 space-y-2 md:hidden'>
+            <li><NavLink to={'/'} className={({ isActive }) => isActive ? 'text-green-500' : ''}>Home</NavLink></li>
+            <li><NavLink to={'/listed'} className={({ isActive }) => isActive ? 'text-green-500' : ''}>Listed Books</NavLink></li>
+            <li><NavLink to={'/pages-read'} className={({ isActive }) => isActive ? 'text-green-500' : ''}>Pages to Read</NavLink></li>
+          </ul>
+          }
         </Container>
       </header>
     </>
